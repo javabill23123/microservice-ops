6c0180fb648e27b9ae19c4cc5dc1115d29030941
haguui <1043398@qq.com>
2018-01-09 18:16:31 +0800

    bug

--- ops-gate/src/main/java/com/yonyou/microservice/gate/server/controller/IgnoreUriController.java
+++ ops-gate/src/main/java/com/yonyou/microservice/gate/server/controller/IgnoreUriController.java
@@ -1,21 +1,26 @@
 package com.yonyou.microservice.gate.server.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.yonyou.microservice.gate.server.filter.AdminAccessFilter;
 
 @RestController
 public class IgnoreUriController {
 	private static Logger logger=Logger.getLogger(IgnoreUriController.class);
 	
+	@ResponseBody
     @RequestMapping(value = "/ignoreuris/refresh", method = RequestMethod.GET)
 	@CacheEvict(value="gate.ignoreuri", allEntries=true)
-    public String cleanIgnoreUriCache() {
+    public Map cleanIgnoreUriCache() {
     	logger.info("--IgnoreUriController cleanIgnoreUriCache was called");
-        return "ok";
+    	Map<String,String> map=new HashMap();
+    	map.put("message", "ok");
+        return map;
     }
 }
