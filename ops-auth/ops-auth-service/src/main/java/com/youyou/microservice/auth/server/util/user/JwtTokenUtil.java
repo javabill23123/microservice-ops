9cc8ddcd717cd2ac77db754e5395754ddb2ff42a
haguui <1043398@qq.com>
2018-01-10 17:29:37 +0800

    log

--- ops-auth/ops-auth-service/src/main/java/com/youyou/microservice/auth/server/util/user/JwtTokenUtil.java
+++ ops-auth/ops-auth-service/src/main/java/com/youyou/microservice/auth/server/util/user/JwtTokenUtil.java
@@ -1,16 +1,19 @@
 package com.youyou.microservice.auth.server.util.user;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.yonyou.cloud.common.jwt.IJwtInfo;
 import com.yonyou.cloud.common.jwt.JwtHelper;
+import com.youyou.microservice.auth.server.service.impl.AuthServiceImpl;
 
 /**
  *  @author joy
  */
 @Component
 public class JwtTokenUtil {
+	private static Logger logger=Logger.getLogger(JwtTokenUtil.class);
 
     @Value("${jwt.expire}")
     private int expire;
@@ -23,6 +26,7 @@ public class JwtTokenUtil {
 //    private RedisTemplate<String, Object> redisTemplate;
 
     public String generateToken(IJwtInfo jwtInfo) throws Exception {
+    	logger.info("--generateToken,expire="+expire);
         return JwtHelper.generateToken(jwtInfo,priKeyPath,expire);
     }
 
