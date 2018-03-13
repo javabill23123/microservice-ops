fc53fd5ec36b881efd198d2c95db4a1277c7af4d
haguui <1043398@qq.com>
2018-03-13 10:02:45 +0800

    网关，redis，踢出登录

--- ops-admin/src/main/java/com/yonyou/microservice/gate/admin/AdminBootstrap.java
+++ ops-admin/src/main/java/com/yonyou/microservice/gate/admin/AdminBootstrap.java
@@ -3,6 +3,7 @@ package com.yonyou.microservice.gate.admin;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
--- ops-gate/src/main/java/com/yonyou/microservice/gate/server/service/CacheService.java
+++ ops-gate/src/main/java/com/yonyou/microservice/gate/server/service/CacheService.java
@@ -1,10 +1,13 @@
 package com.yonyou.microservice.gate.server.service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.yonyou.cloud.common.jwt.IJwtInfo;
@@ -24,6 +27,10 @@ public class CacheService {
     private IUserService userService;
     @Autowired
     private UserAuthUtil userAuthUtil;
+    @Autowired
+    private RedisTemplate redisTemplate;
+	@Value("${redis.expire}")
+    private int expire;
 
     @Cacheable(value = "gate",key="'gate.ignoreuri'")
     public List<IgnoreUriInfo> getIgnoreUris() {
@@ -45,10 +52,59 @@ public class CacheService {
      * @return cache
      * @throws Exception
      */
-    @Cacheable(value = "gate",key="'gate.jwt.'+#authToken")
+//    @Cacheable(value = "gate",key="'gate.jwt.'+#authToken")
     public IJwtInfo getInfoFromToken(String authToken) throws Exception{
-    	logger.info("--getInfoFromToken from jwtUtil"+authToken);
-    	return userAuthUtil.getInfoFromToken(authToken);
+    	logger.info("--getInfoFromToken from jwtUtil,"+authToken);
+    	IJwtInfo result=null;
+    	try{
+        	//根据jwt取jwtInfo
+        	Object jwtInfo=redisTemplate.opsForValue().get(authToken);
+        	if(jwtInfo!=null){
+        		//取得登录账号
+        		String loginName=((IJwtInfo)jwtInfo).getUniqueName();
+        		logger.info("--jwt,loginName="+loginName);
+        		//根据账号取得有效的jwt
+            	Object jwt=redisTemplate.opsForValue().get(loginName);
+            	if(jwt!=null){
+            		//当前jwt与有效jwt是否匹配
+            		boolean r=authToken.equals(jwt);
+            		if(r){
+            			//设置jwt有效期
+            			redisTemplate.expire(authToken, expire, TimeUnit.SECONDS); 
+            			//设置loginName有效期
+            			redisTemplate.expire(loginName, expire, TimeUnit.SECONDS); 
+            			result=(IJwtInfo)jwtInfo;
+                		logger.info("--延长jwt和登录账号有效期"+loginName);
+            		}else{
+                		logger.info("--jwt已经被踢掉,loginName="+loginName+",jwt="+authToken);
+            		}
+            	}else{//根据账号没有找到有效的jwt，则认为当前这个jwt即为有效的jwt
+            		logger.info("--根据登录账号没有找到有效的jwt");
+            		//缓存登录账号有效的jwt
+            		redisTemplate.opsForValue().set(loginName, authToken, expire, TimeUnit.SECONDS); 
+        			//设置jwt有效期
+        			redisTemplate.expire(authToken, expire, TimeUnit.SECONDS); 
+        			result=(IJwtInfo)jwtInfo;
+            		logger.info("--设置账号有效的jwt,loginName="+loginName+",jwt="+authToken);
+            	}
+        	}else{//如果jwt在缓存找不到，则认为这个jwt是新的有效jwt
+        		logger.info("--没有jwt缓存信息");
+        		IJwtInfo info=userAuthUtil.getInfoFromToken(authToken);
+        		//根据jwt缓存用户信息
+        		redisTemplate.opsForValue().set(authToken, info, expire, TimeUnit.SECONDS); 
+        		logger.info("--缓存信息jwt,"+authToken);
+        		String loginName=info.getUniqueName();
+        		//根据用户账号缓存有效的jwt，踢掉其它的jwt
+        		redisTemplate.opsForValue().set(loginName, authToken, expire, TimeUnit.SECONDS); 
+        		logger.info("--用登录账号缓存有效的jwt,loginName="+loginName);
+        		result= info;
+        	}
+    	}catch(Exception e){
+    		logger.info("--redis发生异常，从jwt取信息");
+    		IJwtInfo info=userAuthUtil.getInfoFromToken(authToken);
+    		result= info;
+    	}
+    	return result;
     }
     @Cacheable(value = "gate",key="'gate.permission.user.'+#username")
     public List<PermissionInfo> getPermissionByUsername(String username){
