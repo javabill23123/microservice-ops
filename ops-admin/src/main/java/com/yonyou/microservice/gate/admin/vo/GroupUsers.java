000eb7379c800739e63d9e4343f8b8b77f6a0c13
DESKTOP-ISUL2SA\daniell <daniell_hu@163.com>
2018-03-14 14:26:44 +0800

    添加获取用户组信息以及用户组下用户的接口

--- ops-admin/src/main/java/com/yonyou/microservice/gate/admin/vo/GroupUsers.java
+++ ops-admin/src/main/java/com/yonyou/microservice/gate/admin/vo/GroupUsers.java
@@ -8,15 +8,50 @@ import java.util.List;
  * @author joy
  */
 public class GroupUsers {
+    private Integer id;
+    private String code;
+	private String name;
     List<User> members ;
     List<User> leaders;
+    
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
     public GroupUsers() {
     }
 
     public GroupUsers(List<User> members, List<User> leaders) {
         this.members = members;
+        this.leaders = leaders; 
+    }
+    
+    public GroupUsers(List<User> members, List<User> leaders,int id,String code,String name) {
+        this.members = members;
         this.leaders = leaders;
+        this.id= id;
+        this.name=name;
+        this.code=code;
     }
 
     public List<User> getMembers() {
